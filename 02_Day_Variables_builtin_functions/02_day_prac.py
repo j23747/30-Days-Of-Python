30d7f7b4fd0c3edf806ed7204b5b09b915d29279
jmwania <josphatmwania237@gmail.com>
2021-11-22 14:01:27 +0300

    Practiced on built-in functions

--- 02_Day_Variables_builtin_functions/02_day_prac.py
+++ 02_Day_Variables_builtin_functions/02_day_prac.py
@@ -1,13 +1,20 @@
-# //Practice
+# # Practice
 
 
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
 
-full_name = first_name + " " + last_name
+# full_name = first_name + " " + last_name
 
-print("Hello, " + full_name + "!")
+# print("Hello, " + full_name + "!")
 
 
-person_age = input("Enter your age: ")
-print ("You are " + person_age + " years old.") 
+# person_age = input("Enter your age: ")
+# print ("You are " + person_age + " years old.") 
+
+
+
+hr_personal_info = {'Name':"Hellen Savallah", 'age': 42, 'City': "Nairobi", 'Salary': "100,000"}
+
+print(hr_personal_info)
+print(type(hr_personal_info))
