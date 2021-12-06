3dc0c9312726969626363cec14fbd6592adb4eb1
jmwania <josphatmwania237@gmail.com>
2021-12-06 14:12:51 +0300

    Completed Day 2 Challenge

--- 02_Day_Variables_builtin_functions/02_day_prac.py
+++ 02_Day_Variables_builtin_functions/02_day_prac.py
@@ -1,75 +1,110 @@
-# This is Day 2 Practice
+# # This is Day 2 Practice
 
 
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
 
-full_name = first_name + " " + last_name
+# full_name = first_name + " " + last_name
 
-print("Hello, " + full_name + "!")
-
-
-person_age = input("Enter your age: ")
-print ("You are " + person_age + " years old.") 
+# print("Hello, " + full_name + "!")
 
 
-
-# Dict
-hr_personal_info = {'Name':"Hellen Savallah", 'age': 42, 'City': "Nairobi", 'Salary': "100,000"}
-
-print(hr_personal_info)
-print(type(hr_personal_info))
+# person_age = input("Enter your age: ")
+# print ("You are " + person_age + " years old.") 
 
 
 
-# # int to float
-#  num_int = 10
-# print('num_int',num_int)         # 10
-# num_float = float(num_int)
-# print('num_float:', num_float)   # 10.0
+# # Dict
+# hr_personal_info = {'Name':"Hellen Savallah", 'age': 42, 'City': "Nairobi", 'Salary': "100,000"}
+
+# print(hr_personal_info)
+# print(type(hr_personal_info))
 
 
-# float to int
 
-gravity = 9.34
-print(int(gravity))
-
-# int to str
-num_int = 10
-print(num_int)                  # 10
-num_str = str(num_int)
-print(num_str)                  # '10'
-
-# Declaring Multiple variables
-
-var_country, var_city,  var_city_age, var_city_is_true, var_city_is_light_on = "Kenya", "Nakuru", 30, True, True
-
-print(var_country)
-print(var_city)
-
-### Exercises: Level 2
-# Checking data types
-
-print(type(var_country))
-print(type(var_city))
-print(type(var_city_age))
+# # # int to float
+# #  num_int = 10
+# # print('num_int',num_int)         # 10
+# # num_float = float(num_int)
+# # print('num_float:', num_float)   # 10.0
 
 
-## Finding the length of a string
+# # float to int
 
-print(len(var_city))
-print(len(var_country))
+# gravity = 9.34
+# print(int(gravity))
+
+# # int to str
+# num_int = 10
+# print(num_int)                  # 10
+# num_str = str(num_int)
+# print(num_str)                  # '10'
+
+# # Declaring Multiple variables
+
+# var_country, var_city,  var_city_age, var_city_is_true, var_city_is_light_on = "Kenya", "Nakuru", 30, True, True
+
+# print(var_country)
+# print(var_city)
+
+# ### Exercises: Level 2
+# # Checking data types
+
+# print(type(var_country))
+# print(type(var_city))
+# print(type(var_city_age))
 
 
-# comparing strings length using == operator
+# ## Finding the length of a string
 
-num_1 = "Jay"
-num_2 = "Jonn Wick"
+# print(len(var_city))
+# print(len(var_country))
 
 
-print(num_1 == num_2)
+# # comparing strings length using == operator
 
-if len(num_1) == len(num_2):
-    print("The strings are the same length")
-else:
-    print("The strings are not the same length")
+# num_1 = "Jay"
+# num_2 = "Jonn Wick"
+
+
+# print(num_1 == num_2)
+
+# if len(num_1) == len(num_2):
+#     print("The strings are the same length")
+# else:
+#     print("The strings are not the same length")
+
+
+# Addiding numbers
+
+num_one, num_two = 5, 4
+
+var_total = num_one + num_two
+
+print(var_total)
+
+# SUbstracting numbers
+
+var_diff = num_one - num_two
+
+print(var_diff)
+
+# Multiplying numbers
+var_product = num_one * num_two
+print(var_product)
+
+# Dividing numbers
+var_div = num_one / num_two
+print(var_div)
+
+# modulus
+var_mod = num_one % num_two
+print(var_mod)
+
+# exponent/ calculate num_one to the power of num_two
+var_exp = num_one ** num_two
+print(var_exp)
+
+# Floor division
+var_floor = num_one // num_two
+print(var_floor)
