5acbdd62ddee6f8b9feb306f85f47eb1825fc194
jmwania <josphatmwania237@gmail.com>
2021-12-06 16:57:28 +0300

    Add day 03 challenge

---
+++ 03_Day_Operators/03_day_prac.py
@@ -0,0 +1,19 @@
+### Day 03 CHALLENGE PRACTICE ###
+
+## Declare your age as integer variable
+var_my_age = 30
+
+## Declare your height as a float variable
+var_my_age = floating_point_number = 30.0
+
+print(var_my_age)
+print(type(var_my_age))
+
+
+##  complex number
+
+var_complex_number = complex(1 + 2)
+
+print(var_complex_number)
+
+print(type(var_complex_number))
