089fecd3623d8ad619b27b2763fddafde36678d5
jmwania <josphatmwania237@gmail.com>
2021-12-06 13:15:20 +0300

    Solved Day 2 challenge

--- 02_Day_Variables_builtin_functions/02_day_prac.py
+++ 02_Day_Variables_builtin_functions/02_day_prac.py
@@ -1,20 +1,40 @@
-# # Practice
+# This is Day 2 Practice
 
 
-# first_name = input("Enter your first name: ")
-# last_name = input("Enter your last name: ")
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
 
-# full_name = first_name + " " + last_name
+full_name = first_name + " " + last_name
 
-# print("Hello, " + full_name + "!")
-
-
-# person_age = input("Enter your age: ")
-# print ("You are " + person_age + " years old.") 
+print("Hello, " + full_name + "!")
 
 
+person_age = input("Enter your age: ")
+print ("You are " + person_age + " years old.") 
 
+
+# Dict
 hr_personal_info = {'Name':"Hellen Savallah", 'age': 42, 'City': "Nairobi", 'Salary': "100,000"}
 
 print(hr_personal_info)
 print(type(hr_personal_info))
+
+
+
+# # int to float
+#  num_int = 10
+# print('num_int',num_int)         # 10
+# num_float = float(num_int)
+# print('num_float:', num_float)   # 10.0
+
+
+# float to int
+
+gravity = 9.34
+print(int(gravity))
+
+# int to str
+num_int = 10
+print(num_int)                  # 10
+num_str = str(num_int)
+print(num_str)                  # '10'
