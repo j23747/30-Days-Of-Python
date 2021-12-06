6268d576da3741dd216e32eda3c37cf04a73a151
jmwania <josphatmwania237@gmail.com>
2021-12-06 17:14:31 +0300

    Added Day 3 Challenge methods

--- 03_Day_Operators/03_day_prac.py
+++ 03_Day_Operators/03_day_prac.py
@@ -17,3 +17,23 @@ var_complex_number = complex(1 + 2)
 print(var_complex_number)
 
 print(type(var_complex_number))
+
+# Area of a triangle
+
+# inpupt_base = int(input("Enter the base of the triangle: "))
+# input_height = int(input("Enter the height of the triangle: "))
+# area_of_triangle = (inpupt_base * input_height) / 2 #or (inpupt_base * input_height) / 0.5
+
+# print("The area of the triangle is: ", area_of_triangle)
+
+var_python = "Python"
+var_dragon = "Dragooon"
+
+# len_python = len(var_python)
+# len_dragon = len(var_dragon)
+
+# print(len("Python")) == len("Dragon")
+
+# print(len(var_dragon)) == len(var_python)
+
+print("Python" == "Python", "Python" == "Dragon")
