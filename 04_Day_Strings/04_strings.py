6e64586bb79e575d0a9e3eca565c72202f55f929
jmwania <josphatmwania237@gmail.com>
2021-12-17 01:53:50 +0300

    add Change all the characters to lowercase letters using _lower()

--- 04_Day_Strings/04_strings.py
+++ 04_Day_Strings/04_strings.py
@@ -1,2 +1,24 @@
 ## String methods
 
+## 1. Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+
+full_string = 'Thirty' + ' Days' + ' Of' + ' Python'
+
+print(full_string)
+
+
+## 2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+
+coding_for_all = 'Coding' + ' For' + ' All'
+
+print(coding_for_all)
+
+
+var_company = 'Coding For All'
+
+print(var_company)
+
+
+print(var_company.swapcase())
+
+## 7. Change all the characters to lowercase letters using _lower()_ method.
