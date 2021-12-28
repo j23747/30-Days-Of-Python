c20ee49871254dc7f758c215f06abb18205c904f
jmwania <josphatmwania237@gmail.com>
2021-12-28 13:37:01 +0300

    Started Day 6 challenge

---
+++ 06_Day_Tuples/06_Day_tuples.py
@@ -0,0 +1,7 @@
+### ethods related to tuples:
+
+- tuple(): to create an empty tuple
+- count(): to count the number of a specified item in a tuple
+- index(): to find the index of a specified item in a tuple
+- + operator: to join two or more tuples and to create a new tuple
+- * operator: to repeat a tuple a specified number of times
