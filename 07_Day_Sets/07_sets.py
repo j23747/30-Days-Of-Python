bc07a0ded7ea8843d4953fd4fe35530b4bed3bc5
jmwania <josphatmwania237@gmail.com>
2022-01-03 16:11:17 +0300

    Add pop method

--- 07_Day_Sets/07_sets.py
+++ 07_Day_Sets/07_sets.py
@@ -5,3 +5,33 @@
 set_01 = set()
 
 set_1 = {1, 2, 3, 4, 5}
+
+## finiding length of set
+
+len(set_1)
+print(set_1)
+print(len(set_1))
+
+
+
+## Adding and item in a set
+set_1.add(6)
+
+print(set_1)
+
+
+## Removing an item from a set
+
+set_1.remove(1)
+
+print(set_1)
+
+## using pop method
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+fruits.pop()  # removes a random item from the set
+print(fruits)
+
+## knowing the removed item/fruit
+removed_fruit = fruits.pop()
+print(fruits)
+
