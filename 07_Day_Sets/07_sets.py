61368f3ed1141b2c3a12fe39744e86734a620c08
jmwania <josphatmwania237@gmail.com>
2022-01-08 07:44:18 +0300

    Add set difference method

--- 07_Day_Sets/07_sets.py
+++ 07_Day_Sets/07_sets.py
@@ -49,9 +49,44 @@ print(fruits)
 st = {'item1', 'item2', 'item3', 'item4'}
 del st
 
-
+## a set of kids names
 
 kids = {'Emma', 'Noah', 'Olivia', 'Liam', 'Ava', 'Sophia', 'Mason', 'Isabella', 'Jacob', 'Mia', 'William'}
 
 del kids
 # print(kids)
+
+## joining sets
+
+st = {'item1', 'item2', 'item3', 'item4'}
+kids = {'Emma', 'Noah', 'Olivia', 'Liam', 'Ava', 'Sophia', 'Mason', 'Isabella', 'Jacob', 'Mia', 'William'}
+
+joined_set = st.union(kids)
+
+print(joined_set)
+
+## Difference between sets
+
+st = {'item1', 'item2', 'item3', 'item4'}
+kids = {'Emma', 'Noah', 'Olivia', 'Liam', 'Ava', 'Sophia', 'Mason', 'Isabella', 'Jacob', 'Mia', 'William'}
+kids.difference(st)
+st.difference(kids)
+
+print(kids.difference(st))
+print(st.difference(kids))
+
+## example 2
+whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+even_numbers = {0, 2, 4, 6, 8, 10}
+whole_numbers.difference(even_numbers) # {1, 3, 5, 7, 9}
+
+print(whole_numbers.difference(even_numbers))
+
+database_languages = {'SQL', 'Python', 'Java', 'C++', 'C#', 'JavaScript', 'PHP', 'C', 'Ruby'}
+programming_languages = {'Python', 'Java', 'C++', 'C#', 'JavaScript', 'PHP', 'C', 'Ruby'}
+
+database_languages.difference(programming_languages)
+programming_languages.difference(database_languages)
+
+print(database_languages.difference(programming_languages))
+
